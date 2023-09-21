a909f915cc70011c145a6d549192b4e058b2a0e2
Resul <145687204+ResulKasym05@users.noreply.github.com>
2023-09-21 10:26:33 -0400

    Add files via upload

---
+++ MyFirstClass.java
@@ -0,0 +1,25 @@
+package myFirstPackage;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+
+		boolean isItGood; // declaration
+		isItGood = true; // assignment
+		
+		char initial = '?';
+		
+		byte num1 = 120;
+		short num2 = 130;
+		int num3 = 140;
+		long num4 = 150;
+		
+		float dec1 = 1.11f;
+		double dec2 =1.11;
+		
+		System.out.println(isItGood);
+		System.out.println(initial);
+		
+	}
+
+}
